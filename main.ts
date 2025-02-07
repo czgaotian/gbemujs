a7596880c6e085b44bba43f9fd3219d7c83d73fb
czgaotian <811460602@qq.com>
2025-02-07 20:44:00 +0100

    next step button for debug

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import GameBoy from "./src";
+import { instructionDisplay, registerFDisplay } from "./src/utils/cpu";
 
 const gb = new GameBoy();
 
@@ -25,6 +26,25 @@ pauseButton.addEventListener('click', () => {
     pauseButton.textContent = 'Pause';
   } else {
     gb.pause();
+    console.log(gb);
     pauseButton.textContent = 'Resume';
   }
+});
+
+const stepButton = document.getElementById('step') as HTMLButtonElement;
+stepButton.addEventListener('click', () => {
+  gb.cpu.step();
+
+  console.log(`${gb.clockCycles} - ${gb.cpu.pc.toString(16).padStart(2, '0')} : ${instructionDisplay(gb.cpu)} (${gb.cpu.opcode.toString(16).padStart(2, '0')
+    } ${gb.busRead(gb.cpu.pc + 1).toString(16).padStart(2, '0')
+    } ${gb.busRead(gb.cpu.pc + 2).toString(16).padStart(2, '0')
+    }) A: ${gb.cpu.a.toString(16).padStart(2, '0')
+    } F: ${registerFDisplay(gb.cpu)
+    } BC: ${gb.cpu.b.toString(16).padStart(2, '0')
+    }${gb.cpu.c.toString(16).padStart(2, '0')
+    } DE: ${gb.cpu.d.toString(16).padStart(2, '0')
+    }${gb.cpu.e.toString(16).padStart(2, '0')
+    } HL: ${gb.cpu.h.toString(16).padStart(2, '0')
+    }${gb.cpu.l.toString(16).padStart(2, '0')
+    }`);
 });
